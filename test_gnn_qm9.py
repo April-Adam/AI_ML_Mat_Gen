ed30454b3a876950afe5795ae239aebccc83373e
azhen7 <adam.ray.zheng@gmail.com>
2024-07-19 00:07:06 -0400

    added comments, clarified code

--- test_gnn_qm9.py
+++ test_gnn_qm9.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow import keras
 import numpy as np
 from fetch_data import qm9_parse, qm9_fetch
 from tensorflow import keras
@@ -16,181 +15,194 @@ qm9_records = qm9_fetch()
 data = qm9_parse(qm9_records)
 
 def convert_record(d):
+    """ Takes a parsed record from the QM9 dataset and extracts the elements, coordinates of the atoms, and the ground truth quantum properties. """
+
     # break up record
-    (e, x), y = d
-    #
-    e = e.numpy()
-    x = x.numpy()
-    r = x[:, :3]
-    # make ohc size larger
-    # so use same node feature
-    # shape later
-    ohc = np.zeros((32, 16))
-    ohc[np.arange(len(e)), e - 1] = 1
-    #return (ohc, r), y.numpy()[13]
-    return (ohc, r), y.numpy()
+    (elems, coords), labels = d
+    elems = elems.numpy() # the atoms in the compound
+    coords = coords.numpy()
+    r = coords[:, :3] # of the 4 values in each element of elems, the first 3 elements are the xyz coordinates
 
-def coordsToDistance(x):
-    #x [N, 3] number of atoms, each atom has (x,y,z) coords
-    #x[:, np.newaxis, :] create [N, 1, 3] 
-    #(x - x[:, np.newaxis, :]) ** 2), [N, N, 3]
-    #for each atom, calculate distances along x,y,z to other atoms(inlcude itself in which distance is 0)
-    #sum x,y,z distance [N,N]
-    #inverse calculation.
-    
-    """convert xyz coordinates to inverse pairwise distance"""
+    # one hot encodes the atoms into a 32x16 np array
+    # 32x16: 32 total atoms in the compound, 16 possible elements per atom
+    one_hot_encode = np.zeros((32, 16))
+    one_hot_encode[np.arange(len(elems)), elems - 1] = 1
+
+    return (one_hot_encode, r), labels.numpy()
+
+def coords_to_distances(x):
+    """ Takes the (x, y, z) coordinates of the atoms and outputs the inverse squared distances between pairs of atoms. """
+
+    # Finding squared distances between pairs of atoms:
+    # x[:, np.newaxis, :]                                               - creates an array with shape = (N, 1, 3)
+    # x - x[:, np.newaxis, :]                                           - finds the difference in the x, y, z coords for each pair of atoms
+    # (x - x[:, np.newaxis, :]) ** 2                                    - finds the squares of the above-mentioned differences
+    # r2 = np.sum((x - x[:, np.newaxis, :]) ** 2, axis=-1)              - finds the sum of these squares - this gets the squared distance between pairs of atoms
     r2 = np.sum((x - x[:, np.newaxis, :]) ** 2, axis=-1)
-    e = np.where(r2 != 0.0, 1 / (r2+sys.float_info.epsilon), 0.0)
-    ret = np.zeros((32, 32))
-    ret[0:x.shape[0], 0:x.shape[0]]=e
 
+    # for each element in `r2`, if nonzero, take its reciprocal (adding sys.float_info.epsilon to prevent divisions by 0)
+    e = np.where(r2 != 0.0, 1 / (r2 + sys.float_info.epsilon), 0.0)
+
+    ret = np.zeros((32, 32))
+    ret[0:x.shape[0], 0:x.shape[0]] = e # copies the values of `e` to the top left N x N square of `ret`
     return ret
 
-for d in data:
-    (nodes, coords), y = convert_record(d)
-    features = np.zeros(graph_feature_len)
-    # edges: [N, N] edge between atoms
-    edges = coordsToDistance(coords)
-    # nodes: [N, 16] expanded to [N, N, 16] for matrix multiplication
-    nodes = np.repeat(nodes[np.newaxis, ...], nodes.shape[0], axis=0)
-
-    break
-
-def Conv_block(inputs, feature_dims, activation):
+def conv_block(inputs, feature_dims, activation):
     # Graph Convolution Layer
-    outputs = keras.layers.Conv1D(feature_dims, 1, activation=None, use_bias=False)(inputs)
+    outputs = keras.layers.Conv1D(feature_dims, kernel_size=1, activation=None, use_bias=False)(inputs)
     outputs = keras.layers.BatchNormalization()(outputs)
     outputs = keras.layers.Activation(activation)(outputs)
     return outputs
 
 def simple_gnn_model():
-    # nodes: [N, N, node_feature_len]
-    # edges: [N, N]
-    nodes = keras.Input((32, 32, node_feature_len))
-    edges = keras.Input((32, 32))
-    features = keras.Input((graph_feature_len))
+    nodes = keras.Input(shape=(32, 32, node_feature_len)) # nodes: shape (32, 32, 16)
+    edges = keras.Input(shape=(32, 32)) # edges: shape (32, 32)
+    features = keras.Input(shape=(graph_feature_len,))
 
-    # out: [N, N, msg_feature_len]
-    out = Conv_block(nodes, msg_feature_len, 'relu')
-    # out: = [N, N, msg_feature_len] . [N, N, 1]
+    # input 1: nodes
+    out = conv_block(nodes, msg_feature_len, 'relu')
 
-    #out = keras.layers.Lambda(lambda x: x[0] * K.expand_dims(x[1], axis=-1))([out, edges])
-    x = K.expand_dims(edges, axis=-1)
-    x = K.repeat_elements(x, msg_feature_len, axis=-1)
+    # input 2: edges
+    # purpose of the below code is to reshape edges for matrix multiplication later on
+    x = tf.expand_dims(edges, axis=-1) # shape (None, 32, 32, 1)
+    # duplicate each inverse square distance to form 16 length tensors per atom
+    x = K.repeat_elements(x, msg_feature_len, axis=-1) # shape (None, 32, 32, 16)
+
+    # scale the outputs of the conv block inversely to the distances between atoms
+    # this means that the farther 2 atoms are, the less their connection affects each other
     out = keras.layers.Multiply()([out, x])
-    # out: [N, msg_feature_len]
-    out = keras.layers.Lambda(lambda x: K.sum(x, axis=2,keepdims=False))(out)
-    # new_nodes: [N, node_feature_len]
-    new_nodes = Conv_block(out, node_feature_len, 'relu')
-    # out: [node_feature_len]
-    out = keras.layers.Lambda(lambda x: K.sum(x, axis=1, keepdims=False))(new_nodes)
-    # out: [graph_feature_len]
-    new_features = keras.layers.Dense(graph_feature_len, use_bias=True)(out)
-    new_features = keras.layers.Lambda(lambda x: x[0] + x[1])([new_features, features])
-    pred =  keras.layers.Dense(16, use_bias=True)(new_features)
 
-    model = keras.Model(inputs = [nodes, edges, features], outputs = [pred])
+    # aggregate information about each atom's relationship with all the other atoms
+    out = keras.layers.Lambda(lambda x: K.sum(x, axis=2, keepdims=False))(out) # shape (None, 32, 16)
+
+    # extract features about atoms through convolution
+    new_nodes = conv_block(out, node_feature_len, 'relu') # shape (None, 32, 16)
+
+    # aggregate information further
+    out = keras.layers.Lambda(lambda x: K.sum(x, axis=1, keepdims=False))(new_nodes) # shape (None, 16)
+
+    # fully connected layer
+    new_features = keras.layers.Dense(graph_feature_len, use_bias=True)(out) # shape (None, 16)
+
+    # below code is for features update -> can add features from previous parts of the model to these features.
+    # currently, features is just a bunch of 0s, so technically this does nothing right now,
+    # but this can be used in the future if we decide to expand the model.
+    # inevitably, we will also need a way to preserve the features from previous parts of the model to use,
+    # but that's for later.
+    new_features = keras.layers.Lambda(lambda x: x[0] + x[1])([new_features, features])
+
+    # output layer
+    # each of the 16 values in the output layer corresponds to one of the quantum properties we're predicting
+    pred = keras.layers.Dense(16, use_bias=True)(new_features)
+
+    model = keras.Model(inputs=[nodes, edges, features], outputs=[pred])
     return model
 
-
+# same as tf.keras.metrics.R2Score but as a function
 def r2_keras(y_true, y_pred):
     res = keras.backend.sum(keras.backend.square(y_true - y_pred))
     total = keras.backend.sum(keras.backend.square(y_true - keras.backend.mean(y_true)))
-    return (1-res/(total + keras.backend.epsilon()))
+    return (1 - res / (total + keras.backend.epsilon())) # add keras.backend.epsilon to ensure no division by 0
 
 class DataGenerator(keras.utils.Sequence):
-    def __init__(self, x_data, y_data, z_data, batch_size):
-        self.x, self.y, self.z = np.array(x_data), np.array(y_data), np.array(z_data)
+    def __init__(self, nodes, edges, y_true, batch_size):
+        self.nodes, self.edges, self.y_true = np.array(nodes), np.array(edges), np.array(y_true)
         self.batch_size = batch_size
-        self.num_batches = np.ceil(len(x_data)/batch_size)
-        self.batch_idx = np.array_split(range(len(x_data)), self.num_batches)
+        self.batches = np.ceil(len(nodes) / batch_size)
+        self.batch_idx = np.array_split(range(len(nodes)), self.batches) # splits into `num_batches` batches
 
     def __len__(self):
         return len(self.batch_idx)
     
     def on_epoch_end(self):
         seed = np.random.randint(5)
-        self.x = shuffle(self.x, random_state=seed)
-        self.y = shuffle(self.y, random_state=seed)
-        self.z = shuffle(self.z, random_state=seed)
+        # re-shuffle data for the next epoch
+        self.nodes = shuffle(self.nodes, random_state=seed)
+        self.edges = shuffle(self.edges, random_state=seed)
+        self.y_true = shuffle(self.y_true, random_state=seed)
 
-    def __getitem__(self,idx):
-        batch_x = self.x[self.batch_idx[idx]]
-        batch_y = self.y[self.batch_idx[idx]]
-        features = np.zeros((len(self.batch_idx[idx]), graph_feature_len))
-        batch_z = self.z[self.batch_idx[idx]]
+    def __getitem__(self, idx):
+        batch_nodes = self.nodes[self.batch_idx[idx]]
+        batch_edges = self.edges[self.batch_idx[idx]]
+        features = np.zeros((len(self.batch_idx[idx]), graph_feature_len)) # this is for feature update in the future
+        batch_ground_truth = self.y_true[self.batch_idx[idx]]
 
-        return [batch_x, batch_y, features], batch_z 
+        return [batch_nodes, batch_edges, features], batch_ground_truth 
 
+# get initial data sets
+shuffled_data = data.shuffle(7000, reshuffle_each_iteration=False) # shuffle the data
+test_set = shuffled_data.take(1000) # test set: 1000
+valid_set = shuffled_data.skip(1000).take(1000) # validation set: 1000
+train_set = shuffled_data.skip(2000).take(5000) # training set: 5000
 
-shuffled_data = data.shuffle(7000, reshuffle_each_iteration=False)
-test_set = shuffled_data.take(1000)
-valid_set = shuffled_data.skip(1000).take(1000)
-train_set = shuffled_data.skip(2000).take(5000)
+def create_nodes_and_edges(set):
+    # nodes
+    nodes = [convert_record(d)[0][0] for d in set]
+    nodes = [np.repeat(d[np.newaxis, ...], d.shape[0], axis=0) for d in nodes] # reshape from (32, 16) to (32, 32, 16)
 
+    # edges
+    coords = [convert_record(d)[0][1] for d in set]
+    edges = [coords_to_distances(d) for d in coords]
+
+    return nodes, edges
+
+# preprocess ground truth labels for training set
 ys = [convert_record(d)[1] for d in train_set]
 train_ym = np.mean(ys)
 train_ys = np.std(ys)
-print("Mean = ", train_ym, "Std =", train_ys)
 
+# normalizes y
 def transform_label(y):
     return (y - train_ym) / train_ys
 
 norm_ys = [transform_label(d) for d in ys]
 
-nodes = [(convert_record(d)[0][0]) for d in train_set]
-nodes = [np.repeat(d[np.newaxis, ...], d.shape[0], axis=0) for d in nodes]
+# create nodes
+nodes, edges = create_nodes_and_edges(train_set)
 
-coords = [(convert_record(d)[0][1]) for d in train_set]
-edges = [coordsToDistance(d) for d in coords]
-
-features = np.zeros((len(nodes), graph_feature_len))
-
-model = simple_gnn_model()
-
-optimizer = keras.optimizers.RMSprop(learning_rate=0.0001)
-
-model.compile(loss='mean_squared_error', optimizer = optimizer, metrics=['mae', r2_keras])
-print(model.summary())
-
+# get the data
 batch_size = 32
 train_generator = DataGenerator(nodes, edges, norm_ys, batch_size)
 
-history = model.fit_generator(generator=train_generator, epochs=100)
+# 1. create the model
+model = simple_gnn_model()
 
+# 2. compile the model
+optimizer = keras.optimizers.RMSprop(learning_rate=0.0001)
+model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mae', r2_keras])
+
+# 3. train the model
+history = model.fit(train_generator, epochs=100)
+
+# save model
+# h5
 model.save('gnn_qm9.h5')
+# tflite
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
-
-# Save the model
 with open('gnn_qm9_model.tflite', 'wb') as f:
   f.write(tflite_model)
-print(model.summary())
 
-fig_acc = plt.figure(figsize=(10,10))
+# plot metrics
+fig_acc = plt.figure(figsize=(10, 10))
 plt.plot(history.history['mae'])
 plt.title('model MAE')
 plt.ylabel('MAE')
 plt.xlabel('epoch')
 plt.legend(['train'], loc='upper left')
 plt.show()
-fig_acc.savefig('gnn_qm9_mae.png', dpi=fig_acc.dpi)
+# fig_acc.savefig('gnn_qm9_mae.png', dpi=fig_acc.dpi)
 
+# nodes and edges of validation set
+nodes, edges = create_nodes_and_edges(test_set)
 
-nodes = [(convert_record(d)[0][0]) for d in valid_set]
-nodes = [np.repeat(d[np.newaxis, ...], d.shape[0], axis=0) for d in nodes]
-print((np.array(nodes)).shape)
-coords = [(convert_record(d)[0][1]) for d in valid_set]
-edges = [coordsToDistance(d) for d in coords]
-print((np.array(edges)).shape)
-
+# get ground truth labels in validation set and normalize
 ys = [convert_record(d)[1] for d in valid_set]
 norm_ys = [transform_label(d) for d in ys]
-norm_ys = np.asarray(norm_ys)
-print(norm_ys.shape)
+norm_ys = np.asarray(norm_ys) # convert to np array
 
 features = np.zeros((len(nodes), graph_feature_len))
-print(features.shape)
 
+# 5. evaluate accuracy
 accuracy = model.evaluate([np.array(nodes), np.array(edges), features], norm_ys)
 print(accuracy)
